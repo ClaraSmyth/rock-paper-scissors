5d84de054de57ff6b19c87cae1be223217bc3fab
Clara Smyth <clarasmyth.work@gmail.com>
2022-05-30 18:51:06 +0100

    Completed function that plays the round

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 const choices = ['rock', 'paper', 'scissors'];
+let playerSelection = 'rock';
+let computerSelection = computerPLay();
 
 function computerPLay() {
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
-console.log(computerPLay())
+function playRound() {
+    if (playerSelection == computerSelection) {
+        return 'Tie';
+    } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        return 'Player Wins';
+    } else if (playerSelection == 'rock' && computerSelection == 'paper') {
+        return 'Computer Wins';
+    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        return 'Player Wins';
+    } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+        return 'Computer Wins';
+    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        return 'Player Wins';
+    } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+        return 'Computer Wins';
+    }
+}
+
+
+console.log(playRound())
