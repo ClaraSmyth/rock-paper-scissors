fccaaef47e6e088620465bf59b65ca0726297b55
Clara Smyth <clarasmyth.work@gmail.com>
2022-06-01 18:55:21 +0100

    Optimised the playRound and playGame functions

--- script.js
+++ script.js
@@ -1,43 +1,33 @@
-const choiceArray = ['rock', 'paper', 'scissors'];
+const buttons = document.querySelectorAll('button');
+const scoreboard = document.querySelector('#scoreboard')
 let playerScore = 0;
 let computerScore = 0;
 let ties = 0;
 
 // This selects a random choice from the Array
 function computerPlay() {
+    const choiceArray = ['rock', 'paper', 'scissors'];
     return choiceArray[Math.floor(Math.random() * choiceArray.length)];
 }
 
 // This plays a round of the game and decides who wins
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
-        return 'Tie';
+        return ties++;
     } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
-        return 'Player Wins';
+        return playerScore++;
     } else if (playerSelection == 'rock' && computerSelection == 'paper') {
-        return 'Computer Wins';
+        return computerScore++;
     } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-        return 'Player Wins';
+        return playerScore++;
     } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-        return 'Computer Wins';
+        return computerScore++;
     } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-        return 'Player Wins';
+        return playerScore++;
     } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-        return 'Computer Wins';
+        return computerScore++;
     }
  }
-
-
-const buttons = document.querySelectorAll('button');
-const scoreboard = document.querySelector('#scoreboard')
-
-
-// Button that triggers the game
-buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        playGame(button.id);
-    });
-});
 
 // Updates the scoreboard
 function updateScoreboard() {
@@ -62,22 +52,21 @@ function resetGame() {
     }
 }
 
+// Button that triggers the game
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playGame(button.id);
+    });
+});
+
 // Plays the game
 function playGame(playerButtonChoice) {
     let playerSelection = playerButtonChoice;
     let computerSelection = computerPlay();
     playRound(playerSelection, computerSelection);
-    if (playRound(playerSelection, computerSelection) == 'Tie') {
-        ties++;
-    } else if (playRound(playerSelection, computerSelection) == 'Player Wins') {
-        playerScore++;
-    } else if (playRound(playerSelection, computerSelection) == 'Computer Wins') {
-        computerScore++;
-    }
     updateScoreboard();
     resetGame();
     console.log(playerSelection, computerSelection)
-    console.log(playRound(playerSelection, computerSelection))
     console.log(playerScore, computerScore, ties);
 }
 
